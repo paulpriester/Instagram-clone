fdbdaba336ccb5dbae803c463cda76df60051b18
paulpriester <paulpriester507@gmail.com>
2017-05-18 23:36:10 -0400

    instaclone

--- client/Controllers/home.js
+++ client/Controllers/home.js
@@ -1,12 +1,16 @@
 angular.module('Instagram')
-  .controller('HomeCtrl', function($scope) {
+  .controller('HomeCtrl', function($scope, $window, $rootScope, $auth) {
 
-    $scope.isAuthenticated = function() {
-      // check if logged in
-    };
+		$scope.isAuthenticated = function() {
+	  		return $auth.isAuthenticated();
+	};
 
-    $scope.linkInstagram = function() {
-      // connect email account with instagram
-    };
+		$scope.linkInstagram = function() {
+		  $auth.link('instagram')
+	      .then(function(response) {
+	      $window.localStorage.currentUser = JSON.stringify(response.data.user);
+	      $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+	    });
+	};
 
   });
--- client/app.js
+++ client/app.js
@@ -1,25 +1,36 @@
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+  .config(function($routeProvider, $authProvider) {
 
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-  .config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    })
+    .when('/signup', {
+      templateUrl: 'views/signup.html',
+      controller: 'SignupCtrl'
+    })
+    .when('/photo/:id', {
+      templateUrl: 'views/detail.html',
+      controller: 'DetailCtrl'
+    })
+    .otherwise('/');
 
-  		$routeProvider
-  .when('/', {
-    templateUrl: 'views/home.html',
-    controller: 'HomeCtrl'
-  })
-  .when('/login', {
-    templateUrl: 'views/login.html',
-    controller: 'LoginCtrl'
-  })
-  .when('/signup', {
-    templateUrl: 'views/signup.html',
-    controller: 'SignupCtrl'
-  })
-  .when('/photo/:id', {
-    templateUrl: 'views/detail.html',
-    controller: 'DetailCtrl'
-  })
-  .otherwise('/');
-
+    $authProvider.loginUrl = 'http://localhost:8080/auth/login';
+    $authProvider.signupUrl = 'http://localhost:8080/auth/signup';
+    $authProvider.oauth2({
+    name: 'instagram',
+    url: 'http://localhost:8080/auth/instagram',
+    redirectUri: 'http://localhost:8080',
+    clientId: 'e66f1636c4bd44629960f7fa4d16efe7',
+    requiredUrlParams: ['scope'],
+    scope: ['likes'],
+    scopeDelimiter: '+',
+    authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+   });
   });
 
